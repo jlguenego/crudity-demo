a713d1caa5ef5c29da1c063f66267e9afb795ce5
Jean-Louis GUÉNÉGO <jlguenego@gmail.com>
2021-11-12 16:33:21 +0100

    dev: added edit

--- front/src/app/routes/stock/stock-routing.module.ts
+++ front/src/app/routes/stock/stock-routing.module.ts
@@ -1,3 +1,4 @@
+import { EditComponent } from './edit/edit.component';
 import { AddComponent } from './add/add.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
@@ -6,6 +7,7 @@ import { StockComponent } from './stock.component';
 const routes: Routes = [
   { path: '', component: StockComponent },
   { path: 'add', component: AddComponent },
+  { path: 'edit', component: EditComponent },
 ];
 
 @NgModule({
--- front/src/app/routes/stock/stock.module.ts
+++ front/src/app/routes/stock/stock.module.ts
@@ -5,9 +5,10 @@ import { CommonModule } from '@angular/common';
 import { StockRoutingModule } from './stock-routing.module';
 import { StockComponent } from './stock.component';
 import { AddComponent } from './add/add.component';
+import { EditComponent } from './edit/edit.component';
 
 @NgModule({
-  declarations: [StockComponent, AddComponent],
+  declarations: [StockComponent, AddComponent, EditComponent],
   imports: [CommonModule, StockRoutingModule, FontAwesomeModule],
 })
 export class StockModule {}
