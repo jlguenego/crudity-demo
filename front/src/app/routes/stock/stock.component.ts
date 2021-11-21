56c0820fc27153c5d111894c6709d0e67a8a7f1a
Jean-Louis GUÉNÉGO <jlguenego@gmail.com>
2021-11-21 14:50:02 +0100

    dev: accessibility

--- front/src/app/routes/stock/stock.component.ts
+++ front/src/app/routes/stock/stock.component.ts
@@ -6,6 +6,7 @@ import {
   faPlus,
   faTrashAlt,
   faPen,
+  faBars,
 } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
@@ -18,6 +19,7 @@ export class StockComponent implements OnInit {
   faPlus = faPlus;
   faTrashAlt = faTrashAlt;
   faPen = faPen;
+  faBars = faBars;
 
   selectedArticles = new Set<Article>();
 
@@ -45,4 +47,6 @@ export class StockComponent implements OnInit {
       this.refresh();
     });
   }
+
+  toggleSidebar() {}
 }
